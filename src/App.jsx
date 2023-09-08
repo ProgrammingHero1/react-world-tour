a50ac31dba2cf588b22b51cdebb4525b3f65452e
Programming Hero <programming.hero1@gmail.com>
2023-09-07 22:19:13 -0500

    added CNAME

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countries from './components/Countries/Countries'
 function App() {
   return (
     <>
-      <h1>React World Tour</h1>
+      <h1>React Awesome Fantastic World Tour!!!</h1>
       <Countries></Countries>
     </>
   )
